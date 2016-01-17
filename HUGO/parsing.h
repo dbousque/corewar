cbe7b30261e47ba7a94e631f6091da71574186cf
Hugo BEAUJOUR <hbeaujou@e3r7p1.42.fr>
2016-01-17 16:36:30 +0100

    push pour .h parsing

--- HUGO/parsing.h
+++ HUGO/parsing.h
@@ -6,7 +6,7 @@
 /*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/17 16:00:06 by hbeaujou          #+#    #+#             */
-/*   Updated: 2016/01/17 16:01:14 by hbeaujou         ###   ########.fr       */
+/*   Updated: 2016/01/17 16:29:20 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,38 @@
 # include "op.h"
 # include <fcntl.h>
 
-extern t_op op_tab[17];
+# define OPCODE 1
+# define INDIR 2
+# define DIRE 3
 
-int		convert_file(char *filename);
-int		cant_read_file(char *filename);
-int		cant_create_file(char *filename);
-int		create_res_file(char *filename, char **filename_res);
+typedef struct s_function	t_function;
+typedef struct s_line		t_line;
+typedef struct s_instruct	t_instruct;
+
+struct			s_function
+{
+	char		*label;
+	t_line		*lines;
+};
+
+struct			s_line
+{
+	int			numero;
+	t_instruct	content;
+	t_line		next;
+};
+
+struct			s_instruct
+{
+	char		*name;
+	int			type;
+	int			opcode;
+	t_instrcut	next;
+};
+
+extern t_op		op_tab[17];
+
+int				convert_file(char *filename);
+int				cant_read_file(char *filename);
+int				cant_create_file(char *filename);
+int				create_res_file(char *filename, char **filename_res);
