4c333a71e8db619359c89e4aff8538b56089f8db
dodo <dbousque@student.42.fr>
2016-01-17 18:02:06 +0100

    started adding conversion functions

--- convert_to_bytecode/main.c
+++ convert_to_bytecode/main.c
@@ -1,27 +1,107 @@
 
-// lol
 
-#include "corewar.h"
+#include "convert_to_bytecode.h"
 
-int		usage_info(void)
+t_function	*get_functions(char *filename)
 {
-	ft_putstr("Usage: ./asm <sourcefile>\n");
-	//ft_putstr("-a : Instead of creating a .cor file, outputs a stripped and");
-	//ft_putendl(" annotated version of the code to the standard output");
-	return (0);
-}
+	t_function	*functions;
 
-int		cant_read_file(char *filename)
-{
-	ft_putstr_fd("Can't read source file ", 2);
-	ft_putendl_fd(filename, 2);
-	return (0);
+	functions = (t_function*)malloc(sizeof(t_function));
+	functions->next = (t_function*)malloc(sizeof(t_function));
+	functions->next->next = (t_function*)malloc(sizeof(t_function));
+	functions->next->next->next = NULL;
+
+	functions->label = "header";
+	functions->lines = (t_line*)malloc(sizeof(t_line));
+	functions->lines->next = (t_line*)malloc(sizeof(t_line));
+	functions->lines->numero = 0;
+	functions->lines->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->lines->content->name = ".name";
+	functions->lines->content->type = HEADER;
+	functions->lines->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->lines->content->next->name = "zork";
+	functions->lines->content->next->type = HEADER;
+	functions->lines->content->next->next = NULL;
+	functions->lines->next->numero = 1;
+	functions->lines->next->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->lines->next->content->name = ".comment";
+	functions->lines->next->content->type = HEADER;
+	functions->lines->next->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->lines->next->content->next->name = "LOL ALIVE";
+	functions->lines->next->content->next->type = HEADER;
+	functions->lines->next->content->next->next = NULL;
+	functions->lines->next->next = NULL;
+
+
+	functions->next->label = "l2";
+	functions->next->lines = (t_line*)malloc(sizeof(t_line));
+	functions->next->lines->next = (t_line*)malloc(sizeof(t_line));
+	functions->next->lines->next->next = NULL;
+
+	functions->next->lines->numero = 0;
+	functions->next->lines->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->content->name = "sti";
+	functions->next->lines->content->type = OPCODE;
+	functions->next->lines->content->opcode = 11;
+	functions->next->lines->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->content->next->name = "r1";
+	functions->next->lines->content->next->type = REG;
+	functions->next->lines->content->next->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->content->next->next->name = "%:live";
+	functions->next->lines->content->next->next->type = DIRE;
+	functions->next->lines->content->next->next->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->content->next->next->next->name = "%1";
+	functions->next->lines->content->next->next->next->type = DIRE;
+	functions->next->lines->content->next->next->next->next = NULL;
+
+	functions->next->lines->next->numero = 1;
+	functions->next->lines->next->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->next->content->name = "and";
+	functions->next->lines->next->content->type = OPCODE;
+	functions->next->lines->next->content->opcode = 6;
+	functions->next->lines->next->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->next->content->next->name = "r1";
+	functions->next->lines->next->content->next->type = REG;
+	functions->next->lines->next->content->next->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->next->content->next->next->name = "%0";
+	functions->next->lines->next->content->next->next->type = DIRE;
+	functions->next->lines->next->content->next->next->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->lines->next->content->next->next->next->name = "r1";
+	functions->next->lines->next->content->next->next->next->type = REG;
+	functions->next->lines->next->content->next->next->next->next = NULL;
+
+	functions->next->next->label = "live";
+	functions->next->next->lines = (t_line*)malloc(sizeof(t_line));
+	functions->next->next->lines->next = (t_line*)malloc(sizeof(t_line));
+	functions->next->next->lines->next->next = NULL;
+
+	functions->next->next->lines->numero = 0;
+	functions->next->next->lines->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->next->lines->content->name = "live";
+	functions->next->next->lines->content->type = OPCODE;
+	functions->next->next->lines->content->opcode = 1;
+	functions->next->next->lines->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->next->lines->content->next->name = "%1";
+	functions->next->next->lines->content->next->type = DIRE;
+	functions->next->next->lines->content->next->next = NULL;
+
+	functions->next->next->lines->next->numero = 1;
+	functions->next->next->lines->next->content = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->next->lines->next->content->name = "zjmp";
+	functions->next->next->lines->next->content->type = OPCODE;
+	functions->next->next->lines->next->content->opcode = 9;
+	functions->next->next->lines->next->content->next = (t_instruct*)malloc(sizeof(t_instruct));
+	functions->next->next->lines->next->content->next->name = "%:live";
+	functions->next->next->lines->next->content->next->type = DIRE;
+	functions->next->next->lines->next->content->next->next = NULL;
+	return (functions);
 }
 
 int		cant_create_file(char *filename)
 {
 	ft_putstr_fd("Failed to create binary file ", 2);
-	ft_putendl_fd(filename, 2);
+	if (filename)
+		ft_putendl_fd(filename, 2);
 	return (0);
 }
 
@@ -50,7 +130,7 @@ int		create_res_file(char *filename, char **filename_res)
 
 	last_point = get_last_point_index(filename);
 	if (!(*filename_res = (char*)malloc(sizeof(char) * (last_point + 5))))
-		return (0);
+		return (-1);
 	(*filename_res)[last_point + 4] = '\0';
 	ft_memcpy(*filename_res, filename, last_point);
 	(*filename_res)[last_point] = '.';
@@ -60,10 +140,100 @@ int		create_res_file(char *filename, char **filename_res)
 	return (open(*filename_res, O_CREAT | O_TRUNC | O_RDWR));
 }
 
-int		main(int argc, char **argv)
+int			write_bytes_to_file(char *filename, t_list *bytes)
 {
+	int		fd;
+	char	**res_filename;
+
+	if (!(res_filename = (char**)malloc(sizeof(char*))))
+		return (cant_create_file(NULL));
+	*res_filename = NULL;
+	if ((fd = create_res_file(filename, res_filename)) == -1)
+		return (cant_create_file(*res_filename));
+	while (bytes)
+	{
+		write(fd, (unsigned char*)bytes->content, 1);
+		bytes = bytes->next;
+	}
+	return (0);
+}
+int			big_error(void)
+{
+	ft_putendl_fd("BIG ERROR", 2);
+	return (0);
+}
+int			add_header_to_bytes(t_function *functions, t_list **bytes_end)
+{
+	t_instruct	*name;
+	t_instruct	*comment;
+
+	if (ft_strcmp(functions->label, "header") != 0)
+		return (big_error());
+	if (ft_strcmp(functions->lines->content->name, NAME_CMD_STRING) == 0
+		&& ft_strcmp(functions->lines->next->content->name, COMMENT_CMD_STRING) == 0)
+	{
+		name = functions->lines->content;
+		comment = functions->lines->next->content;
+	}
+	else if (ft_strcmp(functions->lines->next->content->name, NAME_CMD_STRING) == 0
+		&& ft_strcmp(functions->lines->content->name, COMMENT_CMD_STRING) == 0)
+	{
+		name = functions->lines->next->content;
+		comment = functions->lines->content;
+	}
+	else
+		return (big_error());
+	return (1);
+}
+
+int			add_exec_magic_to_bytes(t_list **bytes, t_list **bytes_end)
+{
+	unsigned int	tmp;
+
+	tmp = COREWAR_EXEC_MAGIC;
+	ft_lstaddend(bytes_end, ft_lstnew(&tmp, sizeof(unsigned int)));
+	bytes = bytes_end;
+}
+
+int			convert_to_binary(t_function *functions, char *filename)
+{
+	t_line		*line;
+	t_instruct	*instruct;
+	t_list		*bytes;
+	t_list		*bytes_end;
+
+	bytes = NULL;
+	bytes_end = NULL;
+	if (!(add_exec_magic_to_bytes(&bytes, &bytes_end)))
+		return (0);
+	if (!(add_header_to_bytes(functions, &bytes_end)))
+		return (0);
+	while (functions)
+	{
+		line = functions->lines;
+		ft_printf("FUNCTION NAME : %s\n", functions->label);
+		while (line)
+		{
+			instruct = line->content;
+			while (instruct)
+			{
+				ft_putendl(instruct->name);
+				instruct = instruct->next;
+			}
+			line = line->next;
+		}
+		functions = functions->next;
+	}
+	return (write_bytes_to_file(filename, bytes));
+}
+
+int			main(int argc, char **argv)
+{
+	t_function	*functions;
+
 	if (argc < 2)
-		return (usage_info());
-	convert_file(argv[argc - 1]);
+		return (0);
+	functions = get_functions(argv[argc - 1]);
+	convert_to_binary(functions, argv[argc - 1]);
 	return (0);
 }
---
+++ convert_to_bytecode/parsing.h
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/17 16:00:06 by hbeaujou          #+#    #+#             */
+/*   Updated: 2016/01/17 16:58:39 by hbeaujou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "libdodo.h"
+# include "op.h"
+# include <fcntl.h>
+
+# define OPCODE 1
+# define INDIR 2
+# define DIRE 3
+# define HEADER 4
+# define REG 5
+
+typedef struct s_function	t_function;
+typedef struct s_line		t_line;
+typedef struct s_instruct	t_instruct;
+
+struct			s_function
+{
+	int			header;
+	char		*label;
+	t_line		*lines;
+	t_function  *next;
+};
+
+struct			s_line
+{
+	int			numero;
+	t_instruct	*content;
+	t_line		*next;
+};
+
+struct			s_instruct
+{
+	char		*name;
+	int			type;
+	int			opcode;
+	t_instruct	*next;
+};
+
+extern t_op		op_tab[17];
+
+int				convert_file(char *filename);
+int				cant_read_file(char *filename);
+int				cant_create_file(char *filename);
+int				create_res_file(char *filename, char **filename_res);
+
+#endif
