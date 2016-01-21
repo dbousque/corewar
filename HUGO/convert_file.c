7c5e795ce9126560c18bf16e793b447e709ff5e0
Hugo BEAUJOUR <hbeaujou@e2r4p4.42.fr>
2016-01-21 13:36:28 +0100

    tout ok pour le parsing

--- HUGO/convert_file.c
+++ HUGO/convert_file.c
@@ -6,7 +6,7 @@
 /*   By: hbeaujou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/17 15:59:39 by hbeaujou          #+#    #+#             */
-/*   Updated: 2016/01/21 12:29:42 by hbeaujou         ###   ########.fr       */
+/*   Updated: 2016/01/21 13:35:48 by hbeaujou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,25 @@ int				error_while_reading(char *filename)
 void	affiche(t_function **file)
 {
 	t_function	*tmp;
+	t_line		*tmp2;
+	t_instruct	*tmp3;
 
 	tmp = *file;
 	while (tmp)
 	{
 		ft_printf("nom : %s\n", tmp->label);
-		if (tmp->label == NULL)
-			ft_printf("VIDE\n");
+		tmp2 = tmp->lines;
+		while (tmp2)
+		{
+			tmp3 = tmp2->content;
+			while (tmp3)
+			{
+				ft_printf("instruct : %s\n", tmp3->name);
+				tmp3 = tmp3->next;
+			}
+			ft_printf("\n");
+			tmp2 = tmp2->next;
+		}
 		tmp = tmp->next;
 	}
 }
